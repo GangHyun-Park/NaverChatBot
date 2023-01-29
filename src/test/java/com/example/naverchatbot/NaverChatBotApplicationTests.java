481bbccf7fd389bb74ed1e94d06f772b265ee6ea
GangHyun-Park <66900601+GangHyun-Park@users.noreply.github.com>
2023-01-29 20:23:32 +0900

    20230129

---
+++ src/test/java/com/example/naverchatbot/NaverChatBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.naverchatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaverChatBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
